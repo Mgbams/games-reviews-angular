2e461529bac608710031cf4d482aacfd2308d24f
Kingsley Mgbams <cmgbams@gmail.com>
2022-02-21 11:29:00 +0100

    PA-38 reviews successfully added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,39 +1,34 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  
-  public logValue : any;
+  public logValue: any;
   private readonly BASE_URL = environment.url;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public authenticate(form: any) {
-
     let value = {
-      pseudonym : form.pseudonym,
-      password : form.password
+      pseudonym: form.pseudonym,
+      password: form.password,
     };
 
-    return this.http.post('http://localhost:8080/api/player/login', value)
-    .subscribe(result => {
-      sessionStorage.setItem('currentUser', JSON.stringify(result));
-      //window.location.reload();
-      console.log(result);
-
-    });
+    return this.http
+      .post('http://localhost:8080/api/player/login', value)
+      .subscribe((result) => {
+        sessionStorage.setItem('currentUser', JSON.stringify(result));
+        window.location.reload();
+      });
   }
 
-  isAuthenticated(): any{
+  isAuthenticated(): any {
     if (sessionStorage.getItem('currentUser')) {
-      this.logValue =sessionStorage.getItem('currentUser');
+      this.logValue = sessionStorage.getItem('currentUser');
       return JSON.parse(this.logValue);
     } else {
       return null;
